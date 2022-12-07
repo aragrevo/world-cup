db6d0946d2cb6b8e34592f9b0c2dee6583678bb3
Eduardo Vergara <eduver_san@hotmail.com>
2022-12-07 08:21:07 -0500

    fix: :lipstick: Fix title colors

--- components/DateTitle.jsx
+++ components/DateTitle.jsx
@@ -8,10 +8,11 @@ const bgColors = {
 
 export const DateTitle = ({date, type}) => {
   const day = new Date(date).getUTCDay();
+  console.log(type);
   return (
     <h3
-      className={`sticky top-28 sm:top-[4.4rem]  shadow-sm shadow-slate-900 bg-slate-800 text-base font-semibold py-1 px-4 z-10 ${
-        type === 'R16' ? 'bg-indigo-900' : type === 'QR' ? 'bg-amber-700' : ''
+      className={`sticky top-28 sm:top-[4.4rem]  shadow-sm shadow-slate-900 text-base font-semibold py-1 px-4 z-10 ${
+        type === 'R16' ? 'bg-indigo-900' : type === 'QR' ? 'bg-amber-700' : 'bg-slate-800'
       }`}>
       {days[day]}, {date}
     </h3>
